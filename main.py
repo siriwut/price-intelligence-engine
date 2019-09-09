8eba08a9bbd8e9ba402a8f4343894007c2c8cc6b
Siriwut Ponwapee (Tape) <siriwut.pw@gmail.com>
2019-09-09 18:40:34 +0700

    wip

---
+++ main.py
@@ -0,0 +1,1 @@
+print('hello intelligen55ce')
---
+++ virtual_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/80142/Projects/price-intelligence-engine/virtual_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
