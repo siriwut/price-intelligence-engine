6f6d26bf7abe1b0743f6bf1cd3e33a0941d938d0
Siriwut Ponwapee (Tape) <siriwut.pw@gmail.com>
2019-09-10 18:16:53 +0700

    setup structure: wip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name="price-intelligence-engine",
+    version="0.1",
+    packages=find_packages(),
+)
