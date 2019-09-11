eda59f50662fdfc5d15acf72384c408300eecde1
Siriwut Ponwapee (Tape) <siriwut.pw@gmail.com>
2019-09-11 18:35:29 +0700

    wip:setup_postgres

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+# with open('README.rst') as f:
+#     readme = f.read()
+#
+# with open('LICENSE') as f:
+#     license = f.read()
 
 setup(
     name="price-intelligence-engine",
