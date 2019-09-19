79e5c832e8b9bb685d5271db82b77ac5f5278b7d
Siriwut Ponwapee (Tape) <siriwut.pw@gmail.com>
2019-09-19 15:18:32 +0700

    extract currency

--- crawler/crawler/spiders/dutyfreejapan.py
+++ crawler/crawler/spiders/dutyfreejapan.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from re import sub
+from decimal import Decimal
+
 
 
 class DutyfreejapanSpider(scrapy.Spider):
@@ -10,14 +13,25 @@ class DutyfreejapanSpider(scrapy.Spider):
 
     def parse(self, response):
         for product_detail in response.css('.productDetail'):
+
+            price_with_currency = product_detail.css('.goods_price ::text').get()
+            name = product_detail.css('.goods_name-bold ::text').get()
+            brand = product_detail.css('.goods_brand ::text').get()
+
+            price = Decimal(sub(r'[^\d.]', '', price_with_currency))
+            currency = price_with_currency[0]
+            sku = product_detail.css('.goods_id span')[0].css('::text').get()
+
             yield {
-                'name': product_detail.css('.goods_name-bold ::text').get(),
-                'brand': product_detail.css('.goods_brand ::text').get(),
-                'price': product_detail.css('.goods_price ::text').get()
+                'name': name,
+                'brand': brand,
+                'price': price,
+                'sku': sku,
+                'currency': currency
             }
 
         for product_page_link in response.css('ul#js_listProduct a.productItem_link'):
             yield response.follow(product_page_link, self.parse)
 
-        for next_page_link in response.css('a.listPager'):
+
 
